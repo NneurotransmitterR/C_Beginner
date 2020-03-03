8510ecb8fd86e52a0b6f103c4acedef1d59afe33
NneurotransmitterR <47296255+NneurotransmitterR@users.noreply.github.com>
2020-03-03 23:17:23 +0800

    Add files via upload

---
+++ newbie sourcecode/二分法求零点.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+
+//二分法求零点在闭区间[0,1]上 
+//f(x)=x^3-x^4+4x^2-1  f(0)=-1<0, f(1)=3>0;
+//double zeropoint(double , double );
+
+//IT IS JUST A POSSIBLE VERSION!!!
+double f(double );
+int main(){
+    double head=0.0, foot=1.0;	
+            /*f-        f+*/
+    
+	do{
+	
+	if (f((head+foot)/2) < -1e-6) {
+	    head = (head+foot)/2;
+	}else if(f((head+foot)/2) > 1e-6) {
+	    foot = (head+foot)/2;
+		}
+	
+   }while(f((head+foot)/2) > 1e-6 || f((head+foot)/2) < -1e-6) ; 
+	
+	printf("%f", (head+foot)/2);
+	return 0;
+} 
+
+
+
+double f(double x) {
+	double y=x*x*x-x*x*x*x+4*x*x-1;
+	return y;
+}
