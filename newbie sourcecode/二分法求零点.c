0ae8c6ddf8bb36e25521d01c2cadf8cadd1aa03d
NneurotransmitterR <47296255+NneurotransmitterR@users.noreply.github.com>
2020-03-05 20:20:03 +0800

    Update 二分法求零点.c

--- newbie sourcecode/二分法求零点.c
+++ newbie sourcecode/二分法求零点.c
@@ -3,21 +3,19 @@
 #include <stdio.h>
 double f(double x);
 void zeropoint(double a, double b);
-int main()
-{
+int main() {
     double a=0.0, b=1.0;
     zeropoint(a, b);
     return 0;
 }
  
  
-double f(double x)
-{
+double f(double x) {
     double y=x*x*x-x*x*x*x+4*x*x-1;    /*f(0)<0, f(1)>0*/ 
     return y;
 }
-void zeropoint(double a, double b)
-{   //再次传入的参数已经变成了新的a, b。 
+void zeropoint(double a, double b) {   //再次传入的参数已经变成了新的a, b。 
+   
     double mid, result;
     mid = (a + b) / 2;
     result = f(mid)*f(a);        /*若两端点函数值乘积为负，则零点位于这个区间，*否则零点位于另一半区间*/
