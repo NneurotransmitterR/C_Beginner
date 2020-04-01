2a39ea7c7be17379e1046e1f698eb8d042568f0a
NneurotransmitterR <47296255+NneurotransmitterR@users.noreply.github.com>
2020-04-01 20:10:01 +0800

    Update 约瑟夫环.c

--- newbie sourcecode/约瑟夫环.c
+++ newbie sourcecode/约瑟夫环.c
@@ -15,11 +15,11 @@ int main() {
          
       printf("请输入总人数n：\n");
       scanf("%d",&n);
-	    printf("请输入报的数m：\n");
-	    scanf("%d",&m);
+      printf("请输入报的数m：\n");
+      scanf("%d",&m);
 
       
-	    a = (int*)malloc(n*sizeof(int));      /*实现c99中int a[n]*/
+      a = (int*)malloc(n*sizeof(int));      /*实现c99中int a[n]*/
 
       for(i=0; i<n; i++)  a[i]=i+1;         /*初始编号 */ 
       
@@ -27,8 +27,8 @@ int main() {
       while (n>1) {
 
           i=(i+ m-1 )%n;                      /*!!!这个公式至关重要，用于算出出列的位置!!!*/ 
-		      printf("第%d个出列的是%d号\n", k, a[i]);
-		      for(j = i+1; j < n; j++)   a[j-1] = a[j];      /*把出列的“踢出”数组，后面的补上*/ 
+          printf("第%d个出列的是%d号\n", k, a[i]);
+	  for(j = i+1; j < n; j++)   a[j-1] = a[j];      /*把出列的“踢出”数组，后面的补上*/ 
           n--;
           k++;
           if(i == n)  i = 0;
