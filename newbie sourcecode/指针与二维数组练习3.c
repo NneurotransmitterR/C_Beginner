6674f032b38fff20ea0285b342ee6c84111c6ec0
K.J.Zhao <47296255+NneurotransmitterR@users.noreply.github.com>
2020-05-19 18:13:02 +0800

    Create 指针与二维数组练习3.c

---
+++ newbie sourcecode/指针与二维数组练习3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+//start
+void fsort(char cname[ ][6], int n)
+{ int k, j;
+ char temp[6];
+ for (k=1; k<n; k++)          /*调试时设置断点*/
+      for(j=0; j<n-k; j++)
+            if (strcmp(*(cname+j), *(cname+j+1))>0)
+             {
+                strcpy(temp, *(cname+j));
+      		    strcpy(*(cname+j), *(cname+j+1));
+      		    strcpy(*(cname+j+1), temp);
+            }
+}
+int main()
+{
+      int i;
+      char cname[5][6]={"Wang","Li","Zhang","Jin","Xian"};
+      char (*p)[6]=cname;
+      fsort(p, 5);
+      for (i=0; i<5; i++)
+      	  printf("%s \n", cname[i]);
+      return 0;
+    
+}	
+//end
