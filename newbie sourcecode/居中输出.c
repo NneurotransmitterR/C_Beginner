1c333d5639f35373fbee20bd81db7f08f0e60ad6
K.J.Zhao <47296255+NneurotransmitterR@users.noreply.github.com>
2020-10-03 16:08:01 +0800

    Update 居中输出.c

--- newbie sourcecode/居中输出.c
+++ newbie sourcecode/居中输出.c
@@ -1,37 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
-void fun(void);
-int main(){
-    fun();
+int main(int argc, char **argv){
+    double a;   
+    scanf("%lf", &a);	    	
+    long long b = a, c=1;    	
+    int i = 0, j = 0;
+    while (b >= 10) {
+	b /= 10;
+	c++;               		//整数部分有几位 
+	}
+    if (c > 8 || b < 0){         //防止超过long int最大值时出现的问题 
+	printf("[%.3f]\n[%.3f]\n[%.3f]\n[%.3f]\n", a, a, a, a);
+    } else {
+	    printf("[%.3f]\n", a);
+	    printf("[%12.3f]\n", a);       //左对齐 
+	    printf("[");
+    	    for (; i<(12-4-c)/2; i++)
+		printf(" ");   
+    	    printf("%.3f", a);
+   	    for (; j<(12-4-c-i); j++)
+    		printf(" ");
+    	    printf("]\n");                 //居中对齐 
+	    printf("[%-12.3f]\n", a);      //右对齐 
+    }
     return 0;
 }
-void fun(){
-//居中输出，当数据宽度为奇数时后面空格多。 
-    double a;   
-    char ch1 = 91, ch2 = 93;
-    scanf("%lf", &a);	    	//123.45678
-    int b = (int) a, c=0;    	//整数部分
-    int i = 0, j = 0;
-    while (b) {
-	b/=10;
-	c++;               		//整数部分有几位 
-    }
-    if (c > 9) {
-	printf("bad input\n");
-	exit(1);
-    }
-    printf("%c", ch1);          //打印[ 
-    for (; i<(12-3-c)/2; i++)
-    	printf(" ");            //打印空格 
-    printf("%.3f", a); 			//打印数据 
-    for (; j<(12-3-c-i); j++)
-    	printf(" ");			//打印空格 
-    printf("%c\n", ch2);		//打印] 
-}
-
-/*测试用例I/O
-123.4567890		[   123.457   ]\n
-99				[   99.000    ]\n
-1428.571		[  1428.571   ]\n
-9666512242.4866231		bad input\n
-*/
