1dfa1ba52473d8feb24b20dd6a55b63ddbe268a8
K.J.Zhao <47296255+NneurotransmitterR@users.noreply.github.com>
2020-10-03 12:46:52 +0800

    Update 居中输出.c

--- newbie sourcecode/居中输出.c
+++ newbie sourcecode/居中输出.c
@@ -6,23 +6,23 @@ int main(){
     return 0;
 }
 void fun(){
-	//居中输出，当数据宽度为奇数时后面空格多。 
-	  double a;   
+//居中输出，当数据宽度为奇数时后面空格多。 
+    double a;   
     char ch1 = 91, ch2 = 93;
     scanf("%lf", &a);	    	//123.45678
-	  int b = (int) a, c=0;    	//整数部分
-	  int i = 0, j = 0;
-	  while (b) {
-		  b/=10;
-	 	  c++;               		//整数部分有几位 
-	  }
-	  if (c > 9) {
-		  printf("bad input\n");
-		  exit(1);
-	  }
-	  printf("%c", ch1);          //打印[ 
+    int b = (int) a, c=0;    	//整数部分
+    int i = 0, j = 0;
+    while (b) {
+	b/=10;
+	c++;               		//整数部分有几位 
+    }
+    if (c > 9) {
+	printf("bad input\n");
+	exit(1);
+    }
+    printf("%c", ch1);          //打印[ 
     for (; i<(12-3-c)/2; i++)
-		  printf(" ");            //打印空格 
+    	printf(" ");            //打印空格 
     printf("%.3f", a); 			//打印数据 
     for (; j<(12-3-c-i); j++)
     	printf(" ");			//打印空格 
